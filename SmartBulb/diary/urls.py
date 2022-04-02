9ffc094b5b244a0b9f3f1867a5c5bd04d9f649f0
p-seungri <qkrtmdfl1103@naver.com>
2022-04-02 19:06:53 +0900

    Feat: 일기 수정 및 삭제 기능 추가

--- SmartBulb/diary/urls.py
+++ SmartBulb/diary/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.main_diary, name='main_diary'),
     path('save_diary/<int:year>/<int:month>/<int:day>', views.save_diary, name='save_diary'),
     path('<int:diary_id>/', views.view_diary, name='view_diary'),
+    path('<int:diary_id>/delete', views.delete_diary, name='delete_diary'),
+    path('<int:diary_id>/edit', views.edit_diary, name='edit_diary'),
 ]
