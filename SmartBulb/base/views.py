129a40777270008f3119fe3e1788533b45630469
p-seungri <qkrtmdfl1103@naver.com>
2022-05-07 13:37:56 +0900

    Fix: 유저 이름 표시 수정

--- SmartBulb/base/views.py
+++ SmartBulb/base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from accounts.models import CustomUser
 
 
 # Create your views here.
@@ -8,6 +9,7 @@ from django.contrib import auth
 
 def home(request):
     if request.user.is_authenticated:
-        return render(request, "home.html", {"validity": 1})
+        return render(request, "home.html", {"validity": 1, "username": request.user})
     else:
         return render(request, "home.html", {"validity": 0})
+
