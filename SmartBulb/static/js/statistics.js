68f5681f2806aebbe203a909d61ba6e34327b404
p-seungri <qkrtmdfl1103@naver.com>
2022-05-09 15:52:43 +0900

    Chore: 기타 사항 수정

--- SmartBulb/static/js/statistics.js
+++ SmartBulb/static/js/statistics.js
@@ -4,7 +4,7 @@ const linkStatistics  = () => {
    const viewYear = date.getFullYear();
    const viewMonth = date.getMonth();
 
-   const link = `<div class="link"><a href="../../diary/statistics/${viewYear}/${viewMonth + 1}">감정 통계 보기</a></div>`;
+   const link = `<a href="../../diary/statistics/${viewYear}/${viewMonth + 1}">감정 통계 보기</a>`;
 
    document.querySelector('#link').innerHTML = link;
 };
